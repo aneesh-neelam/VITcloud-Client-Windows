ff380f9178babb930bf741d74e472259ca788960
Aneesh Neelam <neelam.aneesh@gmail.com>
2015-03-28 15:50:28 +0530

    Added basic UI for selecting media directories

---
+++ VITcloud-UI/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VITcloud_UI
+{
+    class Data
+    {
+        private String hostel;
+        private String block;
+        private String room;
+        private String[] directories;
+
+        public Data(String hostel, String block, String room, String[] directories)
+        {
+            this.hostel = hostel;
+            this.block = block;
+            this.room = room;
+            this.directories = directories;
+        }
+
+        public void Scan()
+        {
+
+        }
+    }
+}
---
+++ VITcloud-UI/Options.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace VITcloud_UI
+{
+    partial class Options
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directory_panel = new System.Windows.Forms.Panel();
+            this.browse_button_1 = new System.Windows.Forms.Button();
+            this.ok_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.room_text = new System.Windows.Forms.TextBox();
+            this.hostel_dropdown = new System.Windows.Forms.ComboBox();
+            this.hostel_panel_label = new System.Windows.Forms.Label();
+            this.hostel_panel = new System.Windows.Forms.Panel();
+            this.directory_panel_label = new System.Windows.Forms.Label();
+            this.block_text = new System.Windows.Forms.TextBox();
+            this.hostel_label = new System.Windows.Forms.Label();
+            this.block_label = new System.Windows.Forms.Label();
+            this.room_label = new System.Windows.Forms.Label();
+            this.directory_1 = new System.Windows.Forms.TextBox();
+            this.directory_2 = new System.Windows.Forms.TextBox();
+            this.browse_button_2 = new System.Windows.Forms.Button();
+            this.directory_3 = new System.Windows.Forms.TextBox();
+            this.browse_button_3 = new System.Windows.Forms.Button();
+            this.directory_4 = new System.Windows.Forms.TextBox();
+            this.browse_button_4 = new System.Windows.Forms.Button();
+            this.directory_panel.SuspendLayout();
+            this.hostel_panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // directory_panel
+            // 
+            this.directory_panel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.directory_panel.Controls.Add(this.directory_4);
+            this.directory_panel.Controls.Add(this.browse_button_4);
+            this.directory_panel.Controls.Add(this.directory_3);
+            this.directory_panel.Controls.Add(this.browse_button_3);
+            this.directory_panel.Controls.Add(this.directory_2);
+            this.directory_panel.Controls.Add(this.browse_button_2);
+            this.directory_panel.Controls.Add(this.directory_1);
+            this.directory_panel.Controls.Add(this.browse_button_1);
+            this.directory_panel.Location = new System.Drawing.Point(28, 127);
+            this.directory_panel.Name = "directory_panel";
+            this.directory_panel.Size = new System.Drawing.Size(405, 187);
+            this.directory_panel.TabIndex = 0;
+            // 
+            // browse_button_1
+            // 
+            this.browse_button_1.Location = new System.Drawing.Point(333, 20);
+            this.browse_button_1.Name = "browse_button_1";
+            this.browse_button_1.Size = new System.Drawing.Size(62, 23);
+            this.browse_button_1.TabIndex = 1;
+            this.browse_button_1.Text = "Browse";
+            this.browse_button_1.UseVisualStyleBackColor = true;
+            this.browse_button_1.Click += new System.EventHandler(this.browse_button_1_Click);
+            // 
+            // ok_button
+            // 
+            this.ok_button.Location = new System.Drawing.Point(339, 332);
+            this.ok_button.Name = "ok_button";
+            this.ok_button.Size = new System.Drawing.Size(105, 35);
+            this.ok_button.TabIndex = 2;
+            this.ok_button.Text = "OK";
+            this.ok_button.UseVisualStyleBackColor = true;
+            this.ok_button.Click += new System.EventHandler(this.ok_button_Click);
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Location = new System.Drawing.Point(12, 332);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(105, 35);
+            this.cancel_button.TabIndex = 3;
+            this.cancel_button.Text = "Cancel";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
+            // 
+            // room_text
+            // 
+            this.room_text.Location = new System.Drawing.Point(330, 9);
+            this.room_text.Name = "room_text";
+            this.room_text.Size = new System.Drawing.Size(72, 20);
+            this.room_text.TabIndex = 4;
+            // 
+            // hostel_dropdown
+            // 
+            this.hostel_dropdown.FormattingEnabled = true;
+            this.hostel_dropdown.Items.AddRange(new object[] {
+            "Boys Hostel",
+            "Girls Hostel"});
+            this.hostel_dropdown.Location = new System.Drawing.Point(46, 9);
+            this.hostel_dropdown.Name = "hostel_dropdown";
+            this.hostel_dropdown.Size = new System.Drawing.Size(85, 21);
+            this.hostel_dropdown.TabIndex = 5;
+            // 
+            // hostel_panel_label
+            // 
+            this.hostel_panel_label.AutoSize = true;
+            this.hostel_panel_label.Location = new System.Drawing.Point(25, 18);
+            this.hostel_panel_label.Name = "hostel_panel_label";
+            this.hostel_panel_label.Size = new System.Drawing.Size(72, 13);
+            this.hostel_panel_label.TabIndex = 6;
+            this.hostel_panel_label.Text = "Hostel Details";
+            // 
+            // hostel_panel
+            // 
+            this.hostel_panel.Controls.Add(this.room_label);
+            this.hostel_panel.Controls.Add(this.block_label);
+            this.hostel_panel.Controls.Add(this.hostel_label);
+            this.hostel_panel.Controls.Add(this.block_text);
+            this.hostel_panel.Controls.Add(this.hostel_dropdown);
+            this.hostel_panel.Controls.Add(this.room_text);
+            this.hostel_panel.Location = new System.Drawing.Point(28, 34);
+            this.hostel_panel.Name = "hostel_panel";
+            this.hostel_panel.Size = new System.Drawing.Size(405, 47);
+            this.hostel_panel.TabIndex = 7;
+            // 
+            // directory_panel_label
+            // 
+            this.directory_panel_label.AutoSize = true;
+            this.directory_panel_label.Location = new System.Drawing.Point(25, 103);
+            this.directory_panel_label.Name = "directory_panel_label";
+            this.directory_panel_label.Size = new System.Drawing.Size(89, 13);
+            this.directory_panel_label.TabIndex = 8;
+            this.directory_panel_label.Text = "Media Directories";
+            // 
+            // block_text
+            // 
+            this.block_text.Location = new System.Drawing.Point(204, 9);
+            this.block_text.Name = "block_text";
+            this.block_text.Size = new System.Drawing.Size(55, 20);
+            this.block_text.TabIndex = 6;
+            // 
+            // hostel_label
+            // 
+            this.hostel_label.AutoSize = true;
+            this.hostel_label.Location = new System.Drawing.Point(3, 12);
+            this.hostel_label.Name = "hostel_label";
+            this.hostel_label.Size = new System.Drawing.Size(37, 13);
+            this.hostel_label.TabIndex = 7;
+            this.hostel_label.Text = "Hostel";
+            // 
+            // block_label
+            // 
+            this.block_label.AutoSize = true;
+            this.block_label.Location = new System.Drawing.Point(164, 12);
+            this.block_label.Name = "block_label";
+            this.block_label.Size = new System.Drawing.Size(34, 13);
+            this.block_label.TabIndex = 8;
+            this.block_label.Text = "Block";
+            // 
+            // room_label
+            // 
+            this.room_label.AutoSize = true;
+            this.room_label.Location = new System.Drawing.Point(292, 12);
+            this.room_label.Name = "room_label";
+            this.room_label.Size = new System.Drawing.Size(35, 13);
+            this.room_label.TabIndex = 9;
+            this.room_label.Text = "Room";
+            // 
+            // directory_1
+            // 
+            this.directory_1.Location = new System.Drawing.Point(6, 22);
+            this.directory_1.Name = "directory_1";
+            this.directory_1.Size = new System.Drawing.Size(321, 20);
+            this.directory_1.TabIndex = 0;
+            // 
+            // directory_2
+            // 
+            this.directory_2.Location = new System.Drawing.Point(6, 57);
+            this.directory_2.Name = "directory_2";
+            this.directory_2.Size = new System.Drawing.Size(321, 20);
+            this.directory_2.TabIndex = 2;
+            // 
+            // browse_button_2
+            // 
+            this.browse_button_2.Location = new System.Drawing.Point(333, 55);
+            this.browse_button_2.Name = "browse_button_2";
+            this.browse_button_2.Size = new System.Drawing.Size(62, 23);
+            this.browse_button_2.TabIndex = 3;
+            this.browse_button_2.Text = "Browse";
+            this.browse_button_2.UseVisualStyleBackColor = true;
+            this.browse_button_2.Click += new System.EventHandler(this.browse_button_2_Click);
+            // 
+            // directory_3
+            // 
+            this.directory_3.Location = new System.Drawing.Point(6, 96);
+            this.directory_3.Name = "directory_3";
+            this.directory_3.Size = new System.Drawing.Size(321, 20);
+            this.directory_3.TabIndex = 4;
+            // 
+            // browse_button_3
+            // 
+            this.browse_button_3.Location = new System.Drawing.Point(333, 94);
+            this.browse_button_3.Name = "browse_button_3";
+            this.browse_button_3.Size = new System.Drawing.Size(62, 23);
+            this.browse_button_3.TabIndex = 5;
+            this.browse_button_3.Text = "Browse";
+            this.browse_button_3.UseVisualStyleBackColor = true;
+            this.browse_button_3.Click += new System.EventHandler(this.browse_button_3_Click);
+            // 
+            // directory_4
+            // 
+            this.directory_4.Location = new System.Drawing.Point(6, 136);
+            this.directory_4.Name = "directory_4";
+            this.directory_4.Size = new System.Drawing.Size(321, 20);
+            this.directory_4.TabIndex = 6;
+            // 
+            // browse_button_4
+            // 
+            this.browse_button_4.Location = new System.Drawing.Point(333, 134);
+            this.browse_button_4.Name = "browse_button_4";
+            this.browse_button_4.Size = new System.Drawing.Size(62, 23);
+            this.browse_button_4.TabIndex = 7;
+            this.browse_button_4.Text = "Browse";
+            this.browse_button_4.UseVisualStyleBackColor = true;
+            this.browse_button_4.Click += new System.EventHandler(this.browse_button_4_Click);
+            // 
+            // Options
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(456, 388);
+            this.Controls.Add(this.directory_panel_label);
+            this.Controls.Add(this.hostel_panel);
+            this.Controls.Add(this.hostel_panel_label);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.ok_button);
+            this.Controls.Add(this.directory_panel);
+            this.Name = "Options";
+            this.Text = "VITcloud";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.directory_panel.ResumeLayout(false);
+            this.directory_panel.PerformLayout();
+            this.hostel_panel.ResumeLayout(false);
+            this.hostel_panel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel directory_panel;
+        private System.Windows.Forms.Button browse_button_1;
+        private System.Windows.Forms.Button ok_button;
+        private System.Windows.Forms.Button cancel_button;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.TextBox room_text;
+        private System.Windows.Forms.ComboBox hostel_dropdown;
+        private System.Windows.Forms.Label hostel_panel_label;
+        private System.Windows.Forms.Panel hostel_panel;
+        private System.Windows.Forms.Label directory_panel_label;
+        private System.Windows.Forms.TextBox block_text;
+        private System.Windows.Forms.Label hostel_label;
+        private System.Windows.Forms.Label room_label;
+        private System.Windows.Forms.Label block_label;
+        private System.Windows.Forms.TextBox directory_1;
+        private System.Windows.Forms.TextBox directory_4;
+        private System.Windows.Forms.Button browse_button_4;
+        private System.Windows.Forms.TextBox directory_3;
+        private System.Windows.Forms.Button browse_button_3;
+        private System.Windows.Forms.TextBox directory_2;
+        private System.Windows.Forms.Button browse_button_2;
+    }
+}
+
